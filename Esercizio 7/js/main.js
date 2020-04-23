caa76a6a077966eb8de781cbc02849f74ee3b67a
sabrinagalesi <sabrinagalesi8@gmail.com>
2020-04-23 09:54:29 +0200

    Continuato su esercizio 7

--- Esercizio 7/js/main.js
+++ Esercizio 7/js/main.js
@@ -9,6 +9,22 @@ Con difficoltà 0=> tra 1 e 100, con difficoltà 1 =>  tra 1 e 80, con difficolt
 // In seguito, fare un for che chieda 16 volte all'utente di inserire un numero; sempre compreso tra 1 e 100;
 // Fare un if dicendo che se il numero inserito è uguale a un numero proibito generato, la partita termina, altrimenti il ciclo continua. 
 
+function mina (listaMine, richiesta) { //Faccio una funzione per listare  l'array di mine 
+    var trovato = false;
+    for(var i = 0; i < listaMine.length; i++) {
+        console.log(listaMine[i]);
+        if (richiesta == listaMine[i]) { //Se il numero della richiesta all'utente coincide con il numero corrente esaminato
+            console.log("campo minato!");
+            trovato = true;
+        } 
+    } 
+    if (trovato == true) { //Facciamo un if con return esterno al for, altrimenti si fermerebbe il ciclo
+        return true;
+    } else {
+        return false;
+    }
+}
+
 var listaMine= []; //faccio un array vuoto in cui pushare la lista dei numeri random;
 
 for(var numeroProibito = 0; numeroProibito < 16; numeroProibito++) { //Faccio un for per generare un numero randomico e pusharlo nell'array per 16 volte
@@ -17,34 +33,24 @@ for(var numeroProibito = 0; numeroProibito < 16; numeroProibito++) { //Faccio un
 
 console.log(listaMine);
 
-var richiesta = parseInt(prompt("Inserisci un numero!"));
+var contatoreTentativi = 0;
 
+var ricercaMina = false; //Impostiamo la ricercaMina uguale a false
 
-function mina (listaMine, richiesta) {
-    for(var i = 0; i < listaMine.length; i++) {
-        console.log(listaMine[i]);
-        if (richiesta == listaMine[i]) {
-            console.log("campo minato!");
-            trovato = true;
-        } else {
-            console.log("Salvo!");
-        }
-    } 
-
-}
-
-var trovato = false;
-while(trovato == false) {
+while(ricercaMina == false) { //Finchè ricercaMina è uguale a false, cioè finchè non hai perso;
     console.log("While salvo!")
-    var richiesta = parseInt(prompt("Inserisci un numero!"));
-
-    trovato = mina(listaMine, richiesta);    
+    var richiesta = parseInt(prompt("Inserisci un numero!"));//Continua a chiedermi un numero
+    ricercaMina = mina(listaMine, richiesta);     //Continua a richiamare la funzione di ricerca
+    contatoreTentativi++
+    console.log(contatoreTentativi, "contatore");
 }
 
-if(trovato == true) {
-    console.log("Hai perso!");
+if(ricercaMina == true) { // Se trovato = true allora hai perso
+    alert("Campo minato! Complimenti! Hai evitato le mine " + contatoreTentativi + " volte!");
 }
 
+
+//Fare in modo che il giocatore non possa ripetere lo stesso numero, poi che non possa non scrivere niente(NaN); far finire il gioco a 84 tentaviti;
 
 
 
